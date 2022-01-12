fd325ff771968764b5c080c45dd1148c382f65a0
Dan Skelly <daskelly@users.noreply.github.com>
2022-01-11 21:07:43 -0500

    check indentation

--- 10x_bam_split_CB.py
+++ 10x_bam_split_CB.py
@@ -14,7 +14,7 @@ def main():
     args = parser.parse_args()
     assert args.barcodes.endswith('.gz')
     assert os.path.exists(args.outdir) and os.path.isdir(args.outdir)
-    
+    #
     # Read through barcodes file to capture CBs that we'll be considering
     bc = set()
     f = gzip.open(args.barcodes)
@@ -22,31 +22,31 @@ def main():
         this_barcode = line.rstrip().decode("utf-8")
         bc.add(this_barcode)
     f.close()
-
+    #
     # Open handles to CB-specific BAM files
     cb_bams = dict()
     sfile = pysam.AlignmentFile(args.bamfile, 'rb')
     for barcode in bc:
         file_path = "{}/{}_full.bam".format(args.outdir, barcode)
         cb_bams[barcode] = pysam.AlignmentFile(file_path, 'wb' ,template=sfile)
-
+    #
     # Read through args.bamfile and write each read to the proper CB-specific BAM
     failed = 0
     for i, aln in enumerate(sfile):
-    	if i % 10000000 == 0:
-    		print("On read {}".format(i))
+        if i % 10000000 == 0:
+            print("On read {}".format(i))
         try:
             cb = aln.get_tag('CB')
         except KeyError:
             failed += 1
             continue
         cb_bams[cb].write(aln)
-
+    #
     # Close everything up nice and tidy
     sfile.close()
     for key in cb_bams:
         cb_bams[key].close()
-
+    #
     print("Found {} alignments that \"failed\" due to having no CB available".format(failed))
 
 if __name__ == "__main__":
